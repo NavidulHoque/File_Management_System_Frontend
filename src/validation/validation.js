4fc7de247ca337f408d4dc553dac483db70fcfa4
navidul hoque <hnavidul@gmail.com>
2024-08-27 12:41:57 +0600

    completed

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,17 +1,27 @@
 /* eslint-disable no-unused-vars */
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const signUp = Yup.object({
-    name: Yup.string().min(4).max(20).required("Please fill up your name"),
-    email: Yup.string().email().required("Please fill up your email"),
-    password: Yup.string().min(8).matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/, "Please provide at least 1 special character").required("Please fill up your password")
-})
+  name: Yup.string().min(4).max(20).required("Please fill up your name"),
+  email: Yup.string().email().required("Please fill up your email"),
+  password: Yup.string()
+    .min(8)
+    .matches(
+      /^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+      "Please provide at least 1 special character"
+    )
+    .matches(
+      /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+      "Please enter at least one number"
+    )
+    .required("Please fill up your password"),
+});
 
 export const signIn = Yup.object({
-    email: Yup.string().email().required("Please fill up your email"),
-    password: Yup.string().required("Please fill up your password")
-})
+  email: Yup.string().email().required("Please fill up your email"),
+  password: Yup.string().required("Please fill up your password"),
+});
 
 export const passwordReset = Yup.object({
-    email: Yup.string().email().required("Please fill up your email"),
-})
+  email: Yup.string().email().required("Please fill up your email"),
+});
